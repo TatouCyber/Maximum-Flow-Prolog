d72ccd27579a2cb677c2133039922163e4244423
paraveman <lucasparaveman@gmail.com>
2026-03-22 10:45:17 +0100

    Rajout de la vérification (à compléter)

--- loader.pl
+++ loader.pl
@@ -1,6 +1,43 @@
 load_graph(File) :-
     read_terms(File, Terms),
+    maplist(is_valid,Terms),
+    include(is_edge,Terms,Edges),
+    include(is_node2,Terms,Nodes),
+    graph_correcte(Edges,Nodes),
     maplist(assertz, Terms).
+
+graph_correcte(/* completer */):-
+    /* completer */ .
+
+is_valid(Term):-
+    is_edge(Term);
+    is_node2(Term);
+    is_source(Term);
+    is_sink(Term).
+
+is_valid(Term):-
+    write("Non valide: "),write(Term),nl,
+    fail.
+
+is_edge(edge(X, Y, C)) :-
+    atom(X),
+    atom(Y),
+    number(C),
+    C >= 0.
+
+is_node2(Term):-
+    is_node1(Term);
+    is_sink(Term);
+    is_source(Term).
+
+is_node1(node(X)):-
+    atom(X).
+
+is_source(source(X)):-
+    atom(X).
+
+is_sink(sink(X)):-
+    atom(X).
 
 read_terms(File, Terms) :-
     open(File, read, Stream),
